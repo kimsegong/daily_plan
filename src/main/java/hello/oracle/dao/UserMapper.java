8015ac2534dca16900fc13841e6b0ab8a554e721
unknown <kimsegong@naver.com>
2024-04-01 20:57:50 +0900

    addMapper

--- src/main/java/hello/oracle/dao/UserMapper.java
+++ src/main/java/hello/oracle/dao/UserMapper.java
@@ -4,9 +4,11 @@ import hello.oracle.dto.UserDto;
 import org.apache.ibatis.annotations.Mapper;
 
 import java.util.Map;
+import java.util.Objects;
 
 @Mapper
 public interface UserMapper {
   public int insertUser(UserDto user);
   public UserDto selectUser(int userId);
+  public Map<String, Objects> updateUser(int userId);
 }
